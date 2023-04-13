a9c6998c996a2d3bf93df679a3cd2ece18570c59
Aleksander Michalec <amichalec999@gmail.com>
2023-04-13 12:44:40 +0200

    Refactor
    and readme update

---
+++ src/ProjectManager.Web/Components/ProjectComponents/MessageContent.razor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Microsoft.JSInterop;
+using ProjectManager.Web;
+using ProjectManager.Web.Shared;
+using BlazorInputFile;
+using System.IO;
+using ProjectManager.Web.ApiModels;
+
+namespace ProjectManager.Web.Components.ProjectComponents;
+
+public partial class MessageContent
+{
+  [Parameter] public List<string> MembersFullnames { get; set; }
+  [Parameter] public ChatMessageComplex Message { get; set; }
+  [Parameter] public UserSimplified[] UsersToMention { get; set; }
+  private UserDetails details;
+}
--- src/ProjectManager.Web/Pages/ProjectPages/ProjectChat.razor.cs
+++ src/ProjectManager.Web/Pages/ProjectPages/ProjectChat.razor.cs
@@ -166,5 +166,4 @@ public partial class ProjectChat
   {
     CurrentChannel = channelId;
   }
-
 }
